2574d7ac83f017f530a756fe3c8349cac870a2a9
pds-37 <official.priyanshu.37@gmail.com>
2025-11-30 15:06:30 +0530

    Fix case-sensitive auth.js route

--- models/aimemorymodel.js
+++ models/aimemorymodel.js
@@ -1,6 +1,6 @@
 import express from "express";
 import auth from "../middleware/auth.js";
-import AIMemory from "../models/aimemory.js";
+import AIMemory from "../models/aimemorymodel.js";
 
 const router = express.Router();
 
